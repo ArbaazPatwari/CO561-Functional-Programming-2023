9b25c125c28e88d7ab4d2c6a61c4e40060987e0c
Arbaaz Patwari <arbaazpatwari@gmail.com>
2023-04-01 14:04:54 +0100

    Starting task 1

--- Program.fs
+++ Program.fs
@@ -1,2 +1,14 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+// Task 1
+type Account = {accountNumber : string; mutable balance: float}
+
+let starter = {accountNumber = "A1X"; balance = 100}
+
+let Withdraw taken = 
+    Console.WriteLine("How much money would you like to take out?")
+    let taken = float(Console.ReadLine())
+    starter.balance <- starter.balance - taken
+    Console.WriteLine($"Your balance is now: {starter.balance}")
+
+let lowerBalance = Withdraw 10
